64292c143efda40762421fd76527a70b0e882552
Georgie <george@fivetran.com>
2016-03-12 16:09:54 -0800

    Java save callback

--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,33 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const JAVA_MODE: vscode.DocumentFilter = { language: 'java', scheme: 'file' };
+
+let diagnosticCollection: vscode.DiagnosticCollection;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(ctx: vscode.ExtensionContext) {
+	diagnosticCollection = vscode.languages.createDiagnosticCollection('java');
+	ctx.subscriptions.push(diagnosticCollection);
+    
+	startBuildOnSaveWatcher(ctx.subscriptions);
+}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-javac" is now active!');
+function startBuildOnSaveWatcher(subscriptions: vscode.Disposable[]) {
+	vscode.workspace.onDidSaveTextDocument(document => {
+		if (document.languageId !== 'java') 
+			return;
+            
+		let javaConfig = vscode.workspace.getConfiguration('java');
+		let textEditor = vscode.window.activeTextEditor;
+        
+        runBuilds(document, javaConfig);
+	}, null, subscriptions);
+}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+function runBuilds(document: vscode.TextDocument, javaConfig: vscode.WorkspaceConfiguration) {
+    console.log('Check ' + document.fileName);
 }
 
 // this method is called when your extension is deactivated
