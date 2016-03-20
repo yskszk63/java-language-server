c261375ec0b8c64ce08322486a8a1aed372c5193
Georgie <george@fivetran.com>
2016-03-20 15:49:56 -0700

    Better installs cript

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,6 @@
-cp -r . ~/.vscode/extensions/vscode-javac
+tsc
+
+mkdir -p ~/.vscode/extensions/vscode-javac
+cp -r ./out ~/.vscode/extensions/vscode-javac/out
+cp -r ./node_modules ~/.vscode/extensions/vscode-javac/node_modules
+cp ./package.json ~/.vscode/extensions/vscode-javac/package.json
