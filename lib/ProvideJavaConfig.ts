784190743402f2cc2c07b4a34c906b3434fe3330
Georgie <george@fivetran.com>
2016-03-20 18:47:11 -0700

    Simplify

--- lib/ProvideJavaConfig.ts
+++ lib/ProvideJavaConfig.ts
@@ -4,23 +4,30 @@ import * as P from 'path';
 
 import {JavaConfig} from './JavaConfig';
 
-var cached: JavaConfig;
+let cached: {[path: string]: JavaConfig} = {};
 
+/**
+ * Provide the latest saved version of javaconfig.json 
+ * in the nearest parent directory of [fileName]
+ */
 export function getConfig(fileName: string) {
-    if (cached == null)
+    if (cached[fileName] == null)
         return refreshConfig(fileName);
     else
-        return cached;
+        return cached[fileName];
 }
 
 function refreshConfig(fileName: string) {
-    cached = F.findJavaConfig(V.workspace.rootPath, fileName);
+    cached[fileName] = F.findJavaConfig(V.workspace.rootPath, fileName);
     
-    return cached;
+    return cached[fileName];
 }
 
+/**
+ * Invalidate cached javaconfig.json files when they are saved
+ */
 export function onSaveConfig(document: V.TextDocument) {
     if (P.basename(document.fileName) === 'javaconfig.json') {
-        refreshConfig(document.fileName);
+        cached[document.fileName] = null;
     }
 }
