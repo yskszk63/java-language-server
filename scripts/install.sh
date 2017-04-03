78e45681da28999cf99d0639249ba5bcf2f63d7f
George Fraser <george@fivetran.com>
2017-04-02 22:22:57 -0700

    Explanation

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 # Installs locally
 # You will need java, maven, vsce, and visual studio code to run this script
+set -e
 
 # Build fat jar
 mvn package 
@@ -8,4 +9,5 @@ mvn package
 vsce package -o build.vsix
 
 # Install vsix
+# Note that this will fail if you already have the plugin installed, in which case you should install it through the UI
 code --install-extension build.vsix
