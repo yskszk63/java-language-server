859d0fc1d33bb69e15bb039171837e26b532d78f
Tom Simmons <tomasimmons@gmail.com>
2018-09-08 00:09:41 -0400

    Fix indentation

--- src/test/java/org/javacs/UrlsTest.java
+++ src/test/java/org/javacs/UrlsTest.java
@@ -11,34 +11,34 @@ import java.net.URL;
 import org.junit.Test;
 
 public class UrlsTest {
-
-  @Test
-  public void pathToUrl_whenPathStartsWithForwardSlash() throws Exception {
-    URL actual = Urls.pathToUrl("/a/b/c");
-    assertThat(actual.getProtocol(), equalTo("file"));
-    assertThat(actual.getPath(), containsString("/a/b/c"));
-  }
-
-  @Test
-  public void pathToUrl_whenPathStartsWithProtocol() throws Exception {
-    URL actual = Urls.pathToUrl("file:///a/b/c");
-    assertThat("file", equalTo(actual.getProtocol()));
-    assertThat(actual.getPath(), containsString("/a/b/c"));
-  }
-
-  @Test
-  public void pathToUrl_whenPathStartsWithDriveLetter_usingForwardSlashes()
-      throws Exception {
-    URL actual = Urls.pathToUrl("c:/a/b/c");
-    assertThat(actual.getProtocol(), equalTo("file"));
-    assertThat(actual.getPath(), containsString("/a/b/c"));
-  }
-
-  @Test
-  public void pathToUrl_whenPathStartsWithDriveLetter_usingBackslashes()
-      throws Exception {
-    URL actual = Urls.pathToUrl("c:\\a\\b\\c");
-    assertThat(actual.getProtocol(), equalTo("file"));
-    assertThat(actual.getPath(), containsString("/a/b/c"));
-  }
+    
+    @Test
+    public void pathToUrl_whenPathStartsWithForwardSlash() throws Exception {
+        URL actual = Urls.pathToUrl("/a/b/c");
+        assertThat(actual.getProtocol(), equalTo("file"));
+        assertThat(actual.getPath(), containsString("/a/b/c"));
+    }
+    
+    @Test
+    public void pathToUrl_whenPathStartsWithProtocol() throws Exception {
+        URL actual = Urls.pathToUrl("file:///a/b/c");
+        assertThat("file", equalTo(actual.getProtocol()));
+        assertThat(actual.getPath(), containsString("/a/b/c"));
+    }
+    
+    @Test
+    public void pathToUrl_whenPathStartsWithDriveLetter_usingForwardSlashes()
+    throws Exception {
+        URL actual = Urls.pathToUrl("c:/a/b/c");
+        assertThat(actual.getProtocol(), equalTo("file"));
+        assertThat(actual.getPath(), containsString("/a/b/c"));
+    }
+    
+    @Test
+    public void pathToUrl_whenPathStartsWithDriveLetter_usingBackslashes()
+    throws Exception {
+        URL actual = Urls.pathToUrl("c:\\a\\b\\c");
+        assertThat(actual.getProtocol(), equalTo("file"));
+        assertThat(actual.getPath(), containsString("/a/b/c"));
+    }
 }
