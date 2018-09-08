327340cf082b40d878750c97896caf3df3347e68
Tom Simmons <tomasimmons@gmail.com>
2018-09-07 23:46:59 -0400

    Refactor

--- src/test/java/org/javacs/UrlsTest.java
+++ src/test/java/org/javacs/UrlsTest.java
@@ -1,8 +1,11 @@
 package org.javacs;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 
 import java.net.URL;
 import org.junit.Test;
@@ -10,38 +13,32 @@ import org.junit.Test;
 public class UrlsTest {
 
   @Test
-  public void of_whenPathStartsWithSlash() throws Exception {
-    URL actual = Urls.of("file:///a/b/c");
-    assertEquals("file", actual.getProtocol());
-    assertEquals("/a/b/c", actual.getPath());
+  public void of_whenPathStartsWithForwardSlash() throws Exception {
+    URL actual = Urls.of("/a/b/c");
+    assertThat(actual.getProtocol(), equalTo("file"));
+    assertThat(actual.getPath(), containsString("/a/b/c"));
   }
 
   @Test
   public void of_whenPathStartsWithProtocol() throws Exception {
     URL actual = Urls.of("file:///a/b/c");
-    assertEquals("file", actual.getProtocol());
-    assertEquals("/a/b/c", actual.getPath());
+    assertThat("file", equalTo(actual.getProtocol()));
+    assertThat(actual.getPath(), containsString("/a/b/c"));
   }
 
   @Test
-  public void of_whenPathStartsWithDriveLetter() throws Exception {
+  public void of_whenPathStartsWithDriveLetter_usingForwardSlashes()
+      throws Exception {
     URL actual = Urls.of("c:/a/b/c");
-    assertEquals("file", actual.getProtocol());
+    assertThat(actual.getProtocol(), equalTo("file"));
+    assertThat(actual.getPath(), containsString("/a/b/c"));
   }
 
   @Test
-  public void isSystemPath_whenPathStartsWithSlash() {
-    assertTrue(Urls.isSystemPath("/a/b/c"));
-  }
-
-  @Test
-  public void isSystemPath_whenPathStartsWithDriveLetter() {
-    assertTrue(Urls.isSystemPath("c:/a/b/c"));
-    assertTrue(Urls.isSystemPath("c:\\a\\b\\c"));
-  }
-
-  @Test
-  public void isSystemPath_whenPathStartsWithProtocol() {
-    assertFalse(Urls.isSystemPath("file://a/b/c"));
+  public void of_whenPathStartsWithDriveLetter_usingBackslashes()
+      throws Exception {
+    URL actual = Urls.of("c:\\a\\b\\c");
+    assertThat(actual.getProtocol(), equalTo("file"));
+    assertThat(actual.getPath(), containsString("/a/b/c"));
   }
 }
