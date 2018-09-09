9998081cd796fbb9cc940f39e72ad20ceaa80552
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2018-09-09 16:20:15 -0400

    Remove unecessary OS-dependent tests.

--- src/test/java/org/javacs/UrlsTest.java
+++ src/test/java/org/javacs/UrlsTest.java
@@ -2,9 +2,6 @@ package org.javacs;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 import java.net.URL;
@@ -16,29 +13,25 @@ public class UrlsTest {
     public void pathToUrl_whenPathStartsWithForwardSlash() throws Exception {
         URL actual = Urls.pathToUrl("/a/b/c");
         assertThat(actual.getProtocol(), equalTo("file"));
-        assertThat(actual.getPath(), containsString("/a/b/c"));
     }
     
     @Test
     public void pathToUrl_whenPathStartsWithProtocol() throws Exception {
         URL actual = Urls.pathToUrl("file:///a/b/c");
-        assertThat("file", equalTo(actual.getProtocol()));
-        assertThat(actual.getPath(), containsString("/a/b/c"));
+        assertThat(actual.getProtocol(), equalTo("file"));
     }
     
     @Test
     public void pathToUrl_whenPathStartsWithDriveLetter_usingForwardSlashes()
-    throws Exception {
+            throws Exception {
         URL actual = Urls.pathToUrl("c:/a/b/c");
         assertThat(actual.getProtocol(), equalTo("file"));
-        assertThat(actual.getPath(), containsString("/a/b/c"));
     }
     
     @Test
     public void pathToUrl_whenPathStartsWithDriveLetter_usingBackslashes()
-    throws Exception {
+            throws Exception {
         URL actual = Urls.pathToUrl("c:\\a\\b\\c");
         assertThat(actual.getProtocol(), equalTo("file"));
-        assertThat(actual.getPath(), containsString("/a/b/c"));
     }
 }
