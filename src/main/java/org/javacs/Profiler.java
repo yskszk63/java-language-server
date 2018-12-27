3832f88a8ff975e011eb7ee3f0cf52440ac7a17a
George Fraser <george@fivetran.com>
2018-12-27 13:09:56 -0800

    Log n-files

--- src/main/java/org/javacs/Profiler.java
+++ src/main/java/org/javacs/Profiler.java
@@ -1,18 +1,21 @@
 package org.javacs;
 
 import com.sun.source.util.*;
+import java.net.URI;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
 import java.util.logging.Logger;
 
 class Profiler implements TaskListener {
+    Set<URI> files = new HashSet<>();
     Map<TaskEvent.Kind, Instant> started = new EnumMap<>(TaskEvent.Kind.class);
     Map<TaskEvent.Kind, Duration> profile = new EnumMap<>(TaskEvent.Kind.class);
 
     @Override
     public void started(TaskEvent e) {
         started.put(e.getKind(), Instant.now());
+        files.add(e.getSourceFile().toUri());
     }
 
     @Override
@@ -33,7 +36,7 @@ class Profiler implements TaskListener {
             var ms = elapsed.getSeconds() * 1000 + elapsed.getNano() / 1000 / 1000;
             lines.add(String.format("%s: %dms", k, ms));
         }
-        LOG.info("Compilation profile:\n\t" + lines);
+        LOG.info(String.format("Compiled %d files:\n\t%s", files.size(), lines));
     }
 
     private static final Logger LOG = Logger.getLogger("main");
