03c11ab291eebe0ffd71a4b391ec692989fdba85
Summers Pittman <secondsun@gmail.com>
2019-01-01 21:27:52 -0500

    Adding support for textDocument/documentLink

--- src/main/java/org/javacs/lsp/DocumentLinkParams.java
+++ src/main/java/org/javacs/lsp/DocumentLinkParams.java
@@ -1,5 +1,12 @@
 package org.javacs.lsp;
 
 public class DocumentLinkParams {
+  public TextDocumentIdentifier textDocument;
 
+  public DocumentLinkParams() {
+  }
+
+  public DocumentLinkParams(TextDocumentIdentifier textDocument) {
+    this.textDocument = textDocument;
+  }
 }
