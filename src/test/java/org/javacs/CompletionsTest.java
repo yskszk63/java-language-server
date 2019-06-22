bfd20beac234f204f9d721f0ea286d48d3d66027
George Fraser <george@fivetran.com>
2019-06-22 10:46:36 -0700

    Un-ignore

--- src/test/java/org/javacs/CompletionsTest.java
+++ src/test/java/org/javacs/CompletionsTest.java
@@ -702,7 +702,6 @@ public class CompletionsTest extends CompletionsBase {
         assertThat("suggests enum constants", suggestions, hasItems("Foo"));
     }
 
-    @Ignore // TODO
     @Test
     public void enumConstantFromSourcePath() {
         var file = "/org/javacs/example/AutocompleteCase.java";
