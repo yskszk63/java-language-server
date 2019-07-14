7703da3b35ad88058c4a53be08c01a89a42bf7f3
George Fraser <george@fivetran.com>
2019-07-14 11:49:14 -0700

    Dead code

--- src/main/java/org/javacs/debug/Event.java
+++ src/main/java/org/javacs/debug/Event.java
@@ -1,12 +1,8 @@
 package org.javacs.debug;
 
-import com.google.gson.JsonObject;
-
 /** A debug adapter initiated event. */
 public class Event extends ProtocolMessage {
     // type: 'event';
     /** Type of event. */
     public String event;
-    /** Event-specific information. */
-    public JsonObject body;
 }
--- src/main/java/org/javacs/debug/Request.java
+++ src/main/java/org/javacs/debug/Request.java
@@ -1,12 +1,8 @@
 package org.javacs.debug;
 
-import com.google.gson.JsonObject;
-
 /** A client or debug adapter initiated request. */
 public class Request extends ProtocolMessage {
     // type: 'request';
     /** The command to execute. */
     public String command;
-    /** Object containing arguments for the command. */
-    public JsonObject arguments;
 }
