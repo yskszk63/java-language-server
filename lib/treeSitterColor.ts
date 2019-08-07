2bce5bad5b5511866d13fcc0e48465dbf9f11e1e
George Fraser <george@fivetran.com>
2019-08-07 09:22:02 -0500

    Fix coloring

--- lib/treeSitterColor.ts
+++ lib/treeSitterColor.ts
@@ -1,31 +1,28 @@
 import * as Parser from 'web-tree-sitter';
 
-export function colorJava(root: Parser.SyntaxNode, visibleRanges: {start: number, end: number}[]): {[scope: string]: Parser.SyntaxNode[]} {
-	const colors: {[scope: string]: Parser.SyntaxNode[]} = {
+export function colorJava(root: Parser.SyntaxNode, visibleRanges: { start: number, end: number }[]): { [scope: string]: Parser.SyntaxNode[] } {
+	const colors: { [scope: string]: Parser.SyntaxNode[] } = {
 		'entity.name.type': [],
 		'entity.name.function': [],
 		'variable': [],
 	};
 	class Scope {
-		private vars = new Map<string, {local:boolean}>();
-		private parent: Scope|null;
+		private vars = new Set<string>();
+		parent: Scope | null;
+		local: boolean;
 
-		constructor(parent: Scope|null) {
+		constructor(parent: Scope | null, local: boolean) {
 			this.parent = parent;
+			this.local = local;
 		}
 
-		declareLocal(id: string) {
+		declare(id: string) {
 			if (this.isRoot()) return;
-			this.vars.set(id, {local:true});
-		}
-
-		declareField(id: string) {
-			if (this.isRoot()) return;
-			this.vars.set(id, {local:false});
+			this.vars.add(id);
 		}
 
 		isLocal(id: string): boolean {
-			if (this.vars.has(id)) return this.vars.get(id)!.local;
+			if (this.vars.has(id)) return this.local;
 			if (this.parent) return this.parent.isLocal(id);
 			return false;
 		}
@@ -34,175 +31,148 @@ export function colorJava(root: Parser.SyntaxNode, visibleRanges: {start: number
 			return this.parent == null;
 		}
 	}
-	function scanProgram(x: Parser.SyntaxNode) {
-		const scope = new Scope(null);
-		for (const child of x.namedChildren) {
-			if (!isVisible(child, visibleRanges)) continue;
-			switch (child.type) {
-				case 'package_declaration':
-				case 'import_declaration':
-					break;
-				case 'class_declaration':
-					scanTopLevelClass(child, scope);
-					break;
-				default:
-					scan(child, scope);
+	let visitedChildren = false;
+	let cursor = root.walk();
+	let scope = new Scope(null, false);
+	let parents = [cursor.nodeType];
+	while (true) {
+		// Advance cursor
+		if (visitedChildren) {
+			if (cursor.gotoNextSibling()) {
+				visitedChildren = false;
+			} else if (cursor.gotoParent()) {
+				parents.pop();
+				if (createsScope(cursor.nodeType)) {
+					scope = scope.parent;
+				}
+				visitedChildren = true;
+			} else {
+				break;
+			}
+		} else {
+			const parent = cursor.nodeType;
+			if (cursor.gotoFirstChild()) {
+				if (createsScope(parent)) {
+					scope = new Scope(scope, createsLocalScope(parent));
+				}
+				parents.push(parent);
+				visitedChildren = false;
+			} else {
+				visitedChildren = true;
 			}
 		}
-	}
-	function scanTopLevelClass(x: Parser.SyntaxNode, scope: Scope) {
-		for (const child of x.children) {
-			if (!isVisible(child, visibleRanges)) continue;
-			scan(x, scope);
-		}
-	}
-	function scan(x: Parser.SyntaxNode, scope: Scope) {
-		switch (x.type) {
-			case 'ERROR':
-				return;
-			case 'constructor_declaration':
-			case 'method_declaration':
-			case 'class_body':
-			case 'method_body':
-			case 'block':
-				scope = new Scope(scope);
+		// Color tokens
+		switch (cursor.nodeType) {
+			case 'import_declaration':
+			case 'package_declaration':
+				// Skip package and import declarations
+				visitedChildren = true;
 				break;
-			case 'lambda_expression':
-				scope = new Scope(scope);
-				for (const child of x.namedChildren) {
-					if (child.equals(x.lastNamedChild)) {
-						scan(child, scope);
-					} else {
-						scanLambdaArgument(child, scope);
-					}
+			case 'class_declaration':
+				// Skip classes that are not visible
+				if (!isVisible(cursor, visibleRanges)) {
+					visitedChildren = true;
 				}
-				return;
-			case 'field_declaration':
-				scanFieldDeclaration(x, scope);
-				return;
-			case 'variable_declarator_id':
-				// Anything which isn't a field, must be a local
-				scope.declareLocal(x.text);
-				return; 
-			case 'class_literal':
-				colors['entity.name.type'].push(x.firstChild);
-				return;
+				break;
+			case 'identifier':
+				const parent = parents[parents.length-1];
+				// If this identifier is part of a variable declaration, declare it in the current scope
+				const isParam = parent == 'inferred_parameters' ||
+					parent == 'lambda_expression' && cursor.currentFieldName() == 'parameters' ||
+					parent == 'formal_parameter' && cursor.currentFieldName() == 'name' ||
+					parent == 'variable_declarator' && cursor.currentFieldName() == 'name' ||
+					parent == 'enhanced_for_statement' && cursor.currentFieldName() == 'name';
+				if (isParam) {
+					scope.declare(cursor.currentNode().text);
+				}
+				// If this identifier is part of a selection or invocation, deal with it when we scan the parent
+				const skip = parent == 'method_invocation' && cursor.currentFieldName() == 'name' || 
+					parent == 'scoped_identifier' || 
+					parent == 'field_access' ||
+					parent == 'method_reference';
+				if (skip) {
+					break;
+				}
+				// If this identifier is the name of a class declaration, or part of a type parameter
+				const isTypeName = parent == 'class_declaration' && cursor.currentFieldName() == 'name'
+					|| parent == 'type_parameter';
+				if (isTypeName) {
+					colors['entity.name.type'].push(cursor.currentNode());
+					break;
+				}
+				// If this identifier is the name in a method or constructor declaration
+				const isMethodName = parent == 'method_declaration' && cursor.currentFieldName() == 'name' ||
+					parent == 'constructor_declarator';
+				if (isMethodName) {
+					colors['entity.name.function'].push(cursor.currentNode());
+					break;
+				}
+				// If this identifier is a reference to a non-local variable, color it
+				const reference = cursor.currentNode().text;
+				if (looksLikeVar(reference) && !scope.isLocal(reference)) {
+					colors['variable'].push(cursor.currentNode());
+				}
+				break;
 			case 'type_identifier':
-				if (x.text != 'var') {
-					colors['entity.name.type'].push(x);
+				if (cursor.currentNode().text != 'var') {
+					colors['entity.name.type'].push(cursor.currentNode()); // TODO use startPosition() / endPosition() instead of currentNode()
 				}
-				break;
-			case 'scoped_identifier':
-				if (!looksLikeType(x.lastNamedChild!.text)) {
-					colors['variable'].push(x.lastNamedChild!);
-				}
-				scan(x.firstNamedChild!, scope);
 				break;
 			case 'field_access':
-				colors['variable'].push(x.lastNamedChild);
-				for (const child of x.namedChildren) {
-					if (!child.equals(x.lastNamedChild)) {
-						scan(child, scope);
-					}
-				}
-				return;
-			case 'method_invocation':
-			case 'method_reference':
-				const select = [];
-				for (const child of x.namedChildren) {
-					switch (child.type) {
-						case 'type_argument':
-						case 'argument_list':
-							scan(child, scope);
-							break;
-						default:
-							select.push(child);
-					}
-				}
-				if (select.length > 1) {
-					// First id can be anything
-					scan(select[0], scope);
-					// Middle ids are usually fields
-					for (var i = 1; i < select.length - 1; i++) {
-						if (select[i].type == 'identifier' && !looksLikeType(select[i].text)) {
-							colors['variable'].push(select[i]);
-						}
-					}
-					// Last id is method name
-				}
-				return;
-			case 'identifier':
-				switch (x.parent!.type) {
-					case 'class_declaration':
-					case 'type_parameter':
-						colors['entity.name.type'].push(x);
-						break;
-					case 'constructor_declarator':
-					case 'method_declaration':
-						colors['entity.name.function'].push(x);
-						break;
-					default:
-						if (!looksLikeType(x.text) && !scope.isLocal(x.text)) {
-							colors['variable'].push(x);
-						}
+			case 'scoped_identifier':
+				const select = cursor.currentNode().namedChild(1);
+				if (looksLikeVar(select.text)) {
+					colors['variable'].push(select);
 				}
 				break;
-			// TODO stop early
-		}
-		for (const child of x.namedChildren) {
-			scan(child, scope)
 		}
 	}
-	function scanLambdaArgument(x: Parser.SyntaxNode, scope: Scope) {
-		switch (x.type) {
-			case 'identifier':
-				scope.declareLocal(x.text);
-				break;
-			case 'type_identifier':
-				if (x.text != 'var') {
-					colors['entity.name.type'].push(x);
-				}
-				break;
-			default:
-				for (const child of x.namedChildren) {
-					scanLambdaArgument(child, scope);
-				}
-		}
-	}
-	function scanFieldDeclaration(x: Parser.SyntaxNode, scope: Scope) {
-		switch (x.type) {
-			case 'variable_declarator_id':
-				scope.declareField(x.text);
-				colors['variable'].push(x);
-				break;
-			case 'type_identifier':
-				if (x.text != 'var') {
-					colors['entity.name.type'].push(x);
-				}
-				break;
-			default:
-				for (const child of x.namedChildren) {
-					scanFieldDeclaration(child, scope)
-				}
-		}
-	}
-	function looksLikeType(id: string) {
-		// ''
-		if (id.length == 0) return false;
-		// 'FOO'
-		if (id.toUpperCase() == id) return false;
-		// 'Foo'
-		if (id[0].toUpperCase() == id[0]) return true;
-		return false;
-	}
-	scanProgram(root);
+	cursor.delete();
 	return colors;
 }
-
-function isVisible(x: Parser.SyntaxNode, visibleRanges: {start: number, end: number}[]) {
-	for (const {start, end} of visibleRanges) {
+// function looksLikeType(id: string) {
+// 	// ''
+// 	if (id.length == 0) return false;
+// 	// 'FOO'
+// 	if (id.toUpperCase() == id) return false;
+// 	// 'Foo'
+// 	if (id[0].toUpperCase() == id[0]) return true;
+// 	return false;
+// }
+function looksLikeVar(id: string) {
+	return id[0].toLowerCase() == id[0];
+}
+function isVisible(x: Parser.TreeCursor, visibleRanges: { start: number, end: number }[]) {
+	for (const { start, end } of visibleRanges) {
 		const overlap = x.startPosition.row <= end + 1 && start - 1 <= x.endPosition.row;
 		if (overlap) return true;
 	}
 	return false;
+}
+function createsScope(nodeType: string) {
+	switch (nodeType) {
+		case 'class_declaration':
+		case 'method_declaration':
+		case 'constructor_declaration':
+		case 'block':
+		case 'lambda_expression':
+		case 'enhanced_for_statement':
+			return true;
+		default:
+			return false;
+	}
+}
+function createsLocalScope(nodeType: string) {
+	switch (nodeType) {
+		case 'class_declaration':
+			return false;
+		case 'method_declaration':
+		case 'constructor_declaration':
+		case 'block':
+		case 'lambda_expression':
+		case 'enhanced_for_statement':
+			return true;
+		default:
+			throw new Error(nodeType + ' does not create a scope');
+	}
 }
