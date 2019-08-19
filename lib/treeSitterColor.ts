54ec5a57e87b4286009f655c08441f80e8bf0bfd
George Fraser <george@fivetran.com>
2019-08-19 17:03:33 -0500

    Simplify coloring

--- lib/treeSitterColor.ts
+++ lib/treeSitterColor.ts
@@ -4,36 +4,9 @@ export function colorJava(root: Parser.SyntaxNode, visibleRanges: { start: numbe
 	const colors: { [scope: string]: Parser.SyntaxNode[] } = {
 		'entity.name.type': [],
 		'entity.name.function': [],
-		'variable': [],
 	};
-	class Scope {
-		private vars = new Set<string>();
-		parent: Scope | null;
-		local: boolean;
-
-		constructor(parent: Scope | null, local: boolean) {
-			this.parent = parent;
-			this.local = local;
-		}
-
-		declare(id: string) {
-			if (this.isRoot()) return;
-			this.vars.add(id);
-		}
-
-		isLocal(id: string): boolean {
-			if (this.vars.has(id)) return this.local;
-			if (this.parent) return this.parent.isLocal(id);
-			return false;
-		}
-
-		isRoot(): boolean {
-			return this.parent == null;
-		}
-	}
 	let visitedChildren = false;
 	let cursor = root.walk();
-	let scope = new Scope(null, false);
 	let parents = [cursor.nodeType];
 	while (true) {
 		// Advance cursor
@@ -42,9 +15,6 @@ export function colorJava(root: Parser.SyntaxNode, visibleRanges: { start: numbe
 				visitedChildren = false;
 			} else if (cursor.gotoParent()) {
 				parents.pop();
-				if (createsScope(cursor.nodeType)) {
-					scope = scope.parent;
-				}
 				visitedChildren = true;
 			} else {
 				break;
@@ -52,14 +22,16 @@ export function colorJava(root: Parser.SyntaxNode, visibleRanges: { start: numbe
 		} else {
 			const parent = cursor.nodeType;
 			if (cursor.gotoFirstChild()) {
-				if (createsScope(parent)) {
-					scope = new Scope(scope, createsLocalScope(parent));
-				}
 				parents.push(parent);
 				visitedChildren = false;
 			} else {
 				visitedChildren = true;
 			}
+		}
+		// Skip nodes that are not visible
+		if (!isVisible(cursor, visibleRanges)) {
+			visitedChildren = true;
+			continue;
 		}
 		// Color tokens
 		switch (cursor.nodeType) {
@@ -68,25 +40,8 @@ export function colorJava(root: Parser.SyntaxNode, visibleRanges: { start: numbe
 				// Skip package and import declarations
 				visitedChildren = true;
 				break;
-			case 'class_declaration':
-				// Skip classes that are not visible
-				if (!isVisible(cursor, visibleRanges)) {
-					visitedChildren = true;
-				}
-				break;
 			case 'identifier':
 				const parent = parents[parents.length-1];
-				// If this identifier is part of a variable declaration, declare it in the current scope
-				const isParam = parent == 'inferred_parameters' ||
-					parent == 'lambda_expression' && cursor.currentFieldName() == 'parameters' ||
-					parent == 'formal_parameter' && cursor.currentFieldName() == 'name' ||
-					parent == 'variable_declarator' && cursor.currentFieldName() == 'name' ||
-					parent == 'enhanced_for_statement' && cursor.currentFieldName() == 'name' ||
-					parent == 'catch_formal_parameter' && cursor.currentFieldName() == 'name' ||
-					parent == 'resource_specification' && cursor.currentFieldName() == 'name';
-				if (isParam) {
-					scope.declare(cursor.currentNode().text);
-				}
 				// If this identifier is part of a selection or invocation, deal with it when we scan the parent
 				const skip = parent == 'method_invocation' && cursor.currentFieldName() == 'name' || 
 					parent == 'scoped_identifier' || 
@@ -109,22 +64,10 @@ export function colorJava(root: Parser.SyntaxNode, visibleRanges: { start: numbe
 					colors['entity.name.function'].push(cursor.currentNode());
 					break;
 				}
-				// If this identifier is a reference to a non-local variable, color it
-				const reference = cursor.currentNode().text;
-				if (looksLikeVar(reference) && !scope.isLocal(reference)) {
-					colors['variable'].push(cursor.currentNode());
-				}
 				break;
 			case 'type_identifier':
 				if (cursor.currentNode().text != 'var') {
 					colors['entity.name.type'].push(cursor.currentNode()); // TODO use startPosition() / endPosition() instead of currentNode()
-				}
-				break;
-			case 'field_access':
-			case 'scoped_identifier':
-				const select = cursor.currentNode().namedChild(1);
-				if (looksLikeVar(select.text)) {
-					colors['variable'].push(select);
 				}
 				break;
 		}
@@ -132,53 +75,10 @@ export function colorJava(root: Parser.SyntaxNode, visibleRanges: { start: numbe
 	cursor.delete();
 	return colors;
 }
-// function looksLikeType(id: string) {
-// 	// ''
-// 	if (id.length == 0) return false;
-// 	// 'FOO'
-// 	if (id.toUpperCase() == id) return false;
-// 	// 'Foo'
-// 	if (id[0].toUpperCase() == id[0]) return true;
-// 	return false;
-// }
-function looksLikeVar(id: string) {
-	return id[0].toLowerCase() == id[0];
-}
 function isVisible(x: Parser.TreeCursor, visibleRanges: { start: number, end: number }[]) {
 	for (const { start, end } of visibleRanges) {
 		const overlap = x.startPosition.row <= end + 1 && start - 1 <= x.endPosition.row;
 		if (overlap) return true;
 	}
 	return false;
-}
-function createsScope(nodeType: string) {
-	switch (nodeType) {
-		case 'class_declaration':
-		case 'method_declaration':
-		case 'constructor_declaration':
-		case 'block':
-		case 'lambda_expression':
-		case 'enhanced_for_statement':
-		case 'catch_clause':
-		case 'try_with_resources_statement':
-			return true;
-		default:
-			return false;
-	}
-}
-function createsLocalScope(nodeType: string) {
-	switch (nodeType) {
-		case 'class_declaration':
-			return false;
-		case 'method_declaration':
-		case 'constructor_declaration':
-		case 'block':
-		case 'lambda_expression':
-		case 'enhanced_for_statement':
-		case 'catch_clause':
-		case 'try_with_resources_statement':
-			return true;
-		default:
-			throw new Error(nodeType + ' does not create a scope');
-	}
 }
