32ee6ccb4fd208efb8ab86a8a2b772738498b7ba
George Fraser <george@fivetran.com>
2019-11-18 22:32:10 -0800

    More rewrite ideas

---
+++ src/main/java/org/javacs/rewrite/ReplaceConstructorWithFactoryMethod.java
@@ -0,0 +1,22 @@
+package org.javacs.rewrite;
+
+import java.nio.file.Path;
+import java.util.Map;
+import org.javacs.lsp.TextEdit;
+
+class ReplaceConstructorWithFactoryMethod implements Rewrite {
+    final String className;
+    final String[] erasedParameterTypes;
+    final String factoryMethodName;
+
+    ReplaceConstructorWithFactoryMethod(String className, String[] erasedParameterTypes, String factoryMethodName) {
+        this.className = className;
+        this.erasedParameterTypes = erasedParameterTypes;
+        this.factoryMethodName = factoryMethodName;
+    }
+
+    @Override
+    public Map<Path, TextEdit[]> rewrite(CompilerProvider compiler) {
+        return CANCELLED;
+    }
+}
